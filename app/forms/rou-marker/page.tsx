54b33127356f58a0b5e7e746f5d2210357379283
Darshan Chobarkar <dchobarkar@gmail.com>
2024-10-27 13:29:53 +0530

    add rou-marker form

---
+++ app/forms/rou-marker/page.tsx
@@ -0,0 +1,14 @@
+import ROUMarkerForm from "@/app/ui/forms/roumarkerform";
+import Header from "@/app/ui/header";
+
+const Page = () => {
+  return (
+    <main>
+      <Header title="ROU Marker Form" />
+
+      <ROUMarkerForm />
+    </main>
+  );
+};
+
+export default Page;
