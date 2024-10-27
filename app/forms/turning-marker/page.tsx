b55d8fd72977ff6cb4a52dbd5e232cd331c2fc7d
Darshan Chobarkar <dchobarkar@gmail.com>
2024-10-27 13:23:55 +0530

    add turning marker form

---
+++ app/forms/turning-marker/page.tsx
@@ -0,0 +1,14 @@
+import TurningMarkerForm from "@/app/ui/forms/turningmarkerform";
+import Header from "@/app/ui/header";
+
+const Page = () => {
+  return (
+    <main>
+      <Header title="Turning Marker Form" />
+
+      <TurningMarkerForm />
+    </main>
+  );
+};
+
+export default Page;
