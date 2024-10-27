8f3e5e26fa7234aef12182f3ae68dfa7d13da9fd
Darshan Chobarkar <dchobarkar@gmail.com>
2024-10-27 13:18:39 +0530

    add ofc-marker form

---
+++ app/forms/ofc-marker/page.tsx
@@ -0,0 +1,14 @@
+import OFCMarkerForm from "@/app/ui/forms/ofcmarkerform";
+import Header from "@/app/ui/header";
+
+const Page = () => {
+  return (
+    <main>
+      <Header title="OFC Marker Form" />
+
+      <OFCMarkerForm />
+    </main>
+  );
+};
+
+export default Page;
