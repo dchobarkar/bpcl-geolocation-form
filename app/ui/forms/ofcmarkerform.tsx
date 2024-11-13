bd2968a8d4ad39dac2eaeaee5a329b206ab289e3
Darshan Chobarkar <dchobarkar@gmail.com>
2024-11-13 15:35:31 +0530

    add submit button

--- app/ui/forms/ofcmarkerform.tsx
+++ app/ui/forms/ofcmarkerform.tsx
@@ -100,6 +100,15 @@ const OFCMarkerForm = () => {
             placeholder="Provide details"
           />
         </div>
+
+        <div className="mt-2 flex justify-end">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </div>
       </div>
     </form>
   );
